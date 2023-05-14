d41dcd888e550e38a42edf5f0b6a6ad91330a5b2
Chatterjee, Tapas <tchatterjee@rednucleus.com>
2023-05-14 21:37:00 +0530

    this is the second commit for version 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Github Deployment.</p>
+        <p>Github Deployment version 2.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
